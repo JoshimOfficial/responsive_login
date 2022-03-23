bc3565996479753b75c722e3c6ebc062591eea90
Joshim Uddin <70097642+JoshimOfficial@users.noreply.github.com>
2022-03-24 05:22:56 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+ let emailListner = document.querySelector("#inputEmail4");
+
+ emailListner.addEventListener("click", ()=> {
+
+    let imageFinder  = document.querySelector("img");
+    imageFinder.src = "./img/typing.svg";
+ })
+
+
+ let passListener = document.querySelector("#inputPassword4");
+
+ passListener.addEventListener("click", ()=> {
+    let imageFinder  = document.querySelector("img");
+    imageFinder.src = "./img/pass.svg";
+ })
