953e58b7ed0693aea75fdf7abf3cecb2c598d410
Joshim Uddin <70097642+JoshimOfficial@users.noreply.github.com>
2022-03-24 16:14:52 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
  let emailListner = document.querySelector("#inputEmail4");
 
- emailListner.addEventListener("click", ()=> {
+ emailListner.addEventListener("keydown", ()=> {
 
-    let imageFinder  = document.querySelector("img");
-    imageFinder.src = "./img/typing.svg";
+   let emailTyping = emailListner.value;
+   if(emailTyping.length > 0) {
+      let mailTypingSvg = document.querySelector("#mail_typing");
+      let defaultSvg = document.querySelector("#default");
+      let passTyping = document.querySelector("#pass_typing");
+   
+      defaultSvg.style.display = "none";
+      mailTypingSvg.style.display = "block";
+      passTyping.style.display = "none";
+   }
+   else {
+      console.log(emailTyping.length)
+   }
+
  })
 
 
  let passListener = document.querySelector("#inputPassword4");
 
- passListener.addEventListener("click", ()=> {
-    let imageFinder  = document.querySelector("img");
-    imageFinder.src = "./img/pass.svg";
+ passListener.addEventListener("keydown", ()=> {
+   let passType = passListener.value;
+   let mailTypingSvg = document.querySelector("#mail_typing");
+   let passTyping = document.querySelector("#pass_typing");
+   let defaultSvg = document.querySelector("#default");
+   
+   if(passType.length > 0) {
+      mailTypingSvg.style.display = "none";
+      defaultSvg.style.display = "none";
+      passTyping.style.display = "block";
+   }
+
  })
